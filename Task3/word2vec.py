82b529f5b9e59a202d086e75a187186809ef9a69
atifahmed <atif.project@gmail.com>
2020-06-13 22:57:37 -0400

    task 3 complete

--- Task3/word2vec.py
+++ Task3/word2vec.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     type = 'analyse'
     inputPath = 'Indian_comments.txt'
     modelPath = 'model'
-    labelPath = 'indian_label_tab.txt'
+    labelPath = 'indian_label_tab.csv'
 
     if type == 'train':
         sentences = Sentences(inputPath)
